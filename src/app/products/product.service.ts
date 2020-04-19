41a08c4dbb6596000fef2dae5a56f181c41bb482
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-04-19 12:13:24 +0200

    Exception Handling

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -15,6 +16,25 @@ export class ProductService{
     }
 
     getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl);
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+          tap(data => console.log('All: ' + JSON.stringify(data))),
+          catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+      // in a real world app, we may send the server to some remote logging infrastructure
+      // instead of just logging it to the console
+      let errorMessage = '';
+      if (err.error instanceof ErrorEvent){
+        // A client-side or network error occured. Handle it accordingly.
+        errorMessage = `An error occured: ${err.error.message}`;
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+      }
+      console.error(errorMessage);
+      return throwError(errorMessage);
     }
 }
